2cfe2ff87b186272bcaac6023f376cbce75251ad
bob <rlp1938@gmail.com>
2016-11-27 04:49:40 +0700

    Simplified the C code considerably.

--- hexsed.c
+++ hexsed.c
@@ -52,15 +52,18 @@ typedef struct sedex {
 	int op;
 	int flen;
 	int rlen;
-	unsigned char *tofind;
-	unsigned char *toreplace;
+	int edcount;
+	char *tofind;
+	char *toreplace;
 } sedex;
 
 static char *eslookup(const char *tofind);
 static void dohelp(int forced);
 static sedex validate_expr(const char *expr);
-static uint8_t hex2char(const char *hh, int *ecode);
 static char *str2hex(const char *str);
+static int validatehexstr(const char *hexstr);
+static char *hex2asc(const char *hexstr);
+static int hexchar2int(const char *hexpair);
 
 int main(int argc, char **argv)
 {
@@ -117,6 +120,24 @@ int main(int argc, char **argv)
 	}//while()
 	// now process the non-option arguments
 
+/*	test for hexchar2int()
+	int i;
+	char *fmt1 = "%d %x %d %d\t";
+	char *fmt2 = "%d %x %d %d\n";
+
+	for (i = 0; i < 256; i++) {
+		char buf[5];
+		char *fmt = (i % 4 == 3)? fmt2:fmt1;
+		sprintf(buf, "%.2x", i);
+		int res = hexchar2int(buf);
+		printf(fmt, i, i, res, res-i);
+		if (i<10) fputs("\t", stdout);
+	}
+	fputs("\n", stdout);
+
+	exit(0);
+*/
+
 	// 1.Check that argv[optind] exists.
 	if (!(argv[optind])) {
 		fprintf(stderr, "No expression provided\n");
@@ -138,14 +159,16 @@ int main(int argc, char **argv)
 		fprintf(stderr, "No such file: %s\n", argv[optind]);
 		dohelp(1);
 	}
-
 	// now do the edits
 	int fcount = 0;
 	fdata mydat = readfile(argv[optind], 0, 0);
 	char *cp = mydat.from;
 	while (cp < mydat.to) {
 		char *found = memmem(cp, mydat.to - cp, mysx.tofind, mysx.flen);
-		if (!found) {
+		int doit;
+		// number of edits may be limited by count.
+		doit = (found && (fcount < mysx.edcount));
+		if (!doit) {
 			fwrite(cp, 1, mydat.to - cp, stdout);
 			cp = mydat.to;
 		} else {
@@ -201,14 +224,22 @@ sedex validate_expr(const char *expr)
 	 * Fill in the sedex struct with proper values from expr or abort
 	 * if anything in expr is malformed.
 	*/
-	char buf[NAME_MAX];
-	size_t len = strlen(expr);
-	if (len > NAME_MAX - 1) {
-		fprintf(stderr, "Input too long:\n%s\n", expr);
-		exit(EXIT_FAILURE);
+	char *buf;
+	char *cp;
+	buf = strdup(expr);
+	sedex mysx = {0};
+	/* Adding the ability to specify a count of patterns to be edited.*/
+	if (buf[0] == '=') {
+		mysx.edcount = strtol(&buf[1], NULL, 10);
+		cp = &buf[1];
+		while (isdigit(*cp)) cp++;
+		char *tmp = strdup(cp);
+		strcpy(buf, tmp);
+		free(tmp);
+	} else {
+		mysx.edcount = INT_MAX;
 	}
-	strcpy(buf, expr);
-
+	size_t len = strlen(buf);
 	// test that expr has properly formed separators and command.
 	int badform = 0;
 	int count = 0;
@@ -232,18 +263,17 @@ sedex validate_expr(const char *expr)
 	}
 	if (buf[len - 1] == 's' && count == 2) {
 		badform = 1;
-	}
+	};
 	if (badform) {
 		fprintf(stderr, "Badly formed expression:\n%s\n", expr);
 		exit(EXIT_FAILURE);
 	}
 
 	// format the hex lists
-	sedex mysx;
 	mysx.op = buf[len - 1];
 	mysx.flen = mysx.rlen = 0;
 	// calculate lengths
-	char *cp = buf;
+	cp = buf;
 	cp++;	// get past initial '/'
 	while ((*cp != '/')) {
 		mysx.flen++;
@@ -269,111 +299,56 @@ sedex validate_expr(const char *expr)
 		exit(EXIT_FAILURE);
 		}
 	}
-
 	// check that user has not obviously fubarred the hex input
 	if (mysx.flen %2 != 0 || mysx.rlen %2 != 0) {
 		fprintf(stderr, "Each hex value must be input as a pair,"
 		" eg 00..0F etc\n, %s\n", expr);
 		exit(EXIT_FAILURE);
 	}
-	mysx.flen /= 2;	// 1 byte out for each ascii hex pair.
-	mysx.rlen /= 2;	// 1 byte out for each ascii hex pair.
 
-	// malloc list space.
-	mysx.tofind = malloc(sizeof(char) * mysx.flen);
-	// I'm not really sure if I should bother about this for
-	// small memory allocations.
-	if (!mysx.tofind) {
-		fprintf(stderr, "Could not allocate %i chars.\n", mysx.flen);
+	// set the find and replace strings and check for non-hex chars.
+	char *tofind, *toreplace;
+	cp = buf;
+	cp++;	// past initial '/'
+	char *ep = strchr(cp, '/');	// content of buf is valid.
+	*ep = 0;
+	tofind = strdup(cp);
+	if (mysx.op == 's') {
+		cp = ep + 1;
+		ep = strchr(cp, '/');
+		*ep = 0;
+		toreplace = strdup(cp);
+	}
+	free(buf);
+	if (validatehexstr(tofind) == -1) {
+		fprintf(stderr, "invalid hex chars tofind: \n %s", tofind);
+		free(tofind);
 		exit(EXIT_FAILURE);
-	}
-
-	if (mysx.rlen) {
-		mysx.toreplace = malloc(sizeof(char) * mysx.rlen);
-		// I'm not really sure if I should bother about this for
-		// small memory allocations.
-		if (!mysx.toreplace) {
-			fprintf(stderr, "Could not allocate %i chars.\n",
-						mysx.rlen);
-			exit(EXIT_FAILURE);
-		}
 	} else {
-		mysx.toreplace = NULL;
+		char *res = hex2asc(tofind);
+		mysx.tofind = strdup(res);
+		free(res);
+		free(tofind);
 	}
 
-	// get the byte values for the string hex values
-	cp = buf + 1;	// char after '/'
-	for (i = 0; i < mysx.flen; i++) {
-		int idx = i + i;
-		char hbuf[3];
-		strncpy(hbuf, cp + idx, 2);
-		hbuf[2] = 0;
-		int ecode;
-		int ret = hex2char(hbuf, &ecode);
-		if (ecode != 0) {
-			fprintf(stderr, "Malformed hex input: %s\n", hbuf);
+	if (mysx.op == 's') {
+		if (validatehexstr(toreplace) == -1) {
+			fprintf(stderr, "invalid hex chars toreplace: \n %s",
+					toreplace);
+			free(toreplace);
+			exit(EXIT_FAILURE);
 		} else {
-			mysx.tofind[i] = ret;
+			char *res = hex2asc(toreplace);
+			mysx.toreplace = strdup(res);
+			free(res);
+			free(toreplace);
 		}
-	} // for(i...)
-	if (mysx.rlen) {
-		cp = strchr(cp, '/');	// between find and replace
-		cp++;	// next char
-		for (i = 0; i < mysx.rlen; i++) {
-			int idx = i + i;
-			char hbuf[3];
-			strncpy(hbuf, cp + idx, 2);
-			hbuf[2] = 0;
-			int ecode;
-			int ret = hex2char(hbuf, &ecode);
-			if (ecode != 0) {
-				fprintf(stderr, "Malformed hex input: %s\n", hbuf);
-			} else {
-				mysx.toreplace[i] = ret;
-			}
-		} // for(i...)
 	}
+	// mysx.[f|r]len is double what it now is, re-assign them.
+	mysx.flen = strlen(mysx.tofind);
+	if (mysx.toreplace) mysx.rlen = strlen(mysx.toreplace);
 	return mysx;
 } // validate_expr()
-
-uint8_t hex2char(const char* hh, int *ecode)
-{
-	char *hexlist[256] = {
-"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B",
-"0C", "0D", "0E", "0F", "10", "11", "12", "13", "14", "15", "16", "17",
-"18", "19", "1A", "1B", "1C", "1D", "1E", "1F", "20", "21", "22", "23",
-"24", "25", "26", "27", "28", "29", "2A", "2B", "2C", "2D", "2E", "2F",
-"30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "3A", "3B",
-"3C", "3D", "3E", "3F", "40", "41", "42", "43", "44", "45", "46", "47",
-"48", "49", "4A", "4B", "4C", "4D", "4E", "4F", "50", "51", "52", "53",
-"54", "55", "56", "57", "58", "59", "5A", "5B", "5C", "5D", "5E", "5F",
-"60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "6A", "6B",
-"6C", "6D", "6E", "6F", "70", "71", "72", "73", "74", "75", "76", "77",
-"78", "79", "7A", "7B", "7C", "7D", "7E", "7F", "80", "81", "82", "83",
-"84", "85", "86", "87", "88", "89", "8A", "8B", "8C", "8D", "8E", "8F",
-"90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "9A", "9B",
-"9C", "9D", "9E", "9F", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7",
-"A8", "A9", "AA", "AB", "AC", "AD", "AE", "AF", "B0", "B1", "B2", "B3",
-"B4", "B5", "B6", "B7", "B8", "B9", "BA", "BB", "BC", "BD", "BE", "BF",
-"C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CA", "CB",
-"CC", "CD", "CE", "CF", "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7",
-"D8", "D9", "DA", "DB", "DC", "DD", "DE", "DF", "E0", "E1", "E2", "E3",
-"E4", "E5", "E6", "E7", "E8", "E9", "EA", "EB", "EC", "ED", "EE", "EF",
-"F0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "FA", "FB",
-"FC", "FD", "FE", "FF"
-					};
-	int i;
-	uint8_t ret;
-	*ecode = 0;
-	for (i = 0; i < 256; i++) {
-		if (strcasecmp(hh, hexlist[i]) == 0) {
-			ret = i;	// done
-			return ret;
-		}
-	}
-	*ecode = -1;	// failed
-	return *ecode;
-}
 
 char *str2hex(const char *str)
 {	/* For each byte in str, return the 2 byte hex code.
@@ -399,4 +374,65 @@ char *str2hex(const char *str)
 		strncpy(&buf[2*idx], res, 2);
 	}
 	return buf;
+}
+
+int validatehexstr(const char *hexstr)
+{
+	char *validhex = "0123456789abcdefABCDEF";
+	size_t len = strlen(hexstr);
+	size_t i;
+	for (i = 0; i < len; i++) {
+		char *cp = strchr(validhex, hexstr[i]);
+		if (!cp) return -1;
+	}
+	return 0;
+}
+
+char *hex2asc(const char *hexstr)
+{	/* take a string of hex ascii represented digits and return the
+	 * normal ascii representation.
+	*/
+	size_t len = strlen(hexstr);
+	char *res = calloc(len / 2 + 1, 1);	// 1 byte result for each 2 in.
+	char wrk[3] = {0};
+	size_t i, idx;
+	for (i=0, idx=0; i < len; idx++, i += 2) {
+		strncpy(wrk, &hexstr[i], 2);
+		char c = hexchar2int(wrk);
+		res[idx] = c;
+	}
+	return res;
+}
+
+int hexchar2int(const char *hexpair)
+{	/* get the int value for hexpair 00, 01, .... ,FE, FF */
+	char test[5] = {0};
+	char *list =
+	"0X000X010X020X030X040X050X060X070X080X090X0A0X0B0X0C0X0D0X0E0X0F"
+	"0X100X110X120X130X140X150X160X170X180X190X1A0X1B0X1C0X1D0X1E0X1F"
+	"0X200X210X220X230X240X250X260X270X280X290X2A0X2B0X2C0X2D0X2E0X2F"
+	"0X300X310X320X330X340X350X360X370X380X390X3A0X3B0X3C0X3D0X3E0X3F"
+	"0X400X410X420X430X440X450X460X470X480X490X4A0X4B0X4C0X4D0X4E0X4F"
+	"0X500X510X520X530X540X550X560X570X580X590X5A0X5B0X5C0X5D0X5E0X5F"
+	"0X600X610X620X630X640X650X660X670X680X690X6A0X6B0X6C0X6D0X6E0X6F"
+	"0X700X710X720X730X740X750X760X770X780X790X7A0X7B0X7C0X7D0X7E0X7F"
+	"0X800X810X820X830X840X850X860X870X880X890X8A0X8B0X8C0X8D0X8E0X8F"
+	"0X900X910X920X930X940X950X960X970X980X990X9A0X9B0X9C0X9D0X9E0X9F"
+	"0XA00XA10XA20XA30XA40XA50XA60XA70XA80XA90XAA0XAB0XAC0XAD0XAE0XAF"
+	"0XB00XB10XB20XB30XB40XB50XB60XB70XB80XB90XBA0XBB0XBC0XBD0XBE0XBF"
+	"0XC00XC10XC20XC30XC40XC50XC60XC70XC80XC90XCA0XCB0XCC0XCD0XCE0XCF"
+	"0XD00XD10XD20XD30XD40XD50XD60XD70XD80XD90XDA0XDB0XDC0XDD0XDE0XDF"
+	"0XE00XE10XE20XE30XE40XE50XE60XE70XE80XE90XEA0XEB0XEC0XED0XEE0XEF"
+	"0XF00XF10XF20XF30XF40XF50XF60XF70XF80XF90XFA0XFB0XFC0XFD0XFE0XFF"
+	;
+	strcpy(test, "0X");
+	test[2] = toupper(hexpair[0]);
+	test[3] = toupper(hexpair[1]);
+	char *cp = strstr(list, test);
+	if (!cp) {
+		fprintf(stderr, "Non-hex input: %s\n", hexpair);
+		exit(EXIT_FAILURE);
+	}
+	int res = (cp - list)/4;
+	return res;
 }
